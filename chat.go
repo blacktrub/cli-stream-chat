def968d9e091099d0fcd3baff17ccea24055c57a
blacktrub <lightlocdog@gmail.com>
2022-02-06 00:30:50 +0300

    add base chat script

---
+++ chat.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/abhinavxd/youtube-live-chat-downloader/v2"
+	"github.com/gempir/go-twitch-irc/v3"
+)
+
+func listenYoutube(wg sync.WaitGroup, streamLink string) {
+	continuation, cfg, error := YtChat.ParseInitialData(streamLink)
+	if error != nil {
+		fmt.Println("error youtube", error)
+	}
+	for {
+		chat, newContinuation, error := YtChat.FetchContinuationChat(continuation, cfg)
+		if error != nil {
+			fmt.Println("error youtube", error)
+			continue
+		}
+		continuation = newContinuation
+		for _, msg := range chat {
+			fmt.Print(msg.Timestamp, " | ")
+			fmt.Println(msg.AuthorName, ": ", msg.Message)
+		}
+	}
+}
+
+func listenTwitch(wg sync.WaitGroup, channelName string) {
+	client := twitch.NewAnonymousClient()
+
+	client.OnPrivateMessage(func(message twitch.PrivateMessage) {
+		fmt.Println(message.Message)
+	})
+
+	client.Join(channelName)
+	err := client.Connect()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func runListeners(twitch string, youtubeLink string) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go listenTwitch(wg, twitch)
+	go listenYoutube(wg, youtubeLink)
+
+	wg.Wait()
+}
+
+func main() {
+	twitch := flag.String("twitch", "", "Twitch channel name")
+	youtubeLink := flag.String("youtube", "", "Youtube stream link")
+	flag.Parse()
+	if *twitch == "" || *youtubeLink == "" {
+		fmt.Println("Bad run arguments")
+		os.Exit(0)
+	}
+	runListeners(*twitch, *youtubeLink)
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module cli-stream-chat
+
+go 1.17
+
+require (
+	github.com/abhinavxd/youtube-live-chat-downloader/v2 v2.0.1 // indirect
+	github.com/gempir/go-twitch-irc/v3 v3.0.0 // indirect
+)
